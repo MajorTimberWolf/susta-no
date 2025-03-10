6af54adbe94e5d6b04ca5f66d3e4a955eb758875
Jai Khurana <64320051+jaikhuranna@users.noreply.github.com>
2025-03-10 09:31:26 +0530

    init

---
+++ Sustano/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sustano
+//
+//  Created by Jai Khurana on 10/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
